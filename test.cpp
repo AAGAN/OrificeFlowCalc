60edfa838e90864aa591f79f8af9f65e1850a1b8
AAGAN <AAGAN@users.noreply.github.com>
2017-06-01 09:58:24 -0400

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,9 +1,23 @@
+#include <iostream>
 #include <assert.h>
-#include <stdio.h>
-#include <OrificeFlowCalc.h>
 
-int main
+int main()
 {
-  
-  return 0;
+    // OrificeFlowCalc function calculates different methods for calculating the flow through an orifice
+    // double beta = nozzle orifice diameter / upstream pipe diameter (dimensionless)
+    // double orifice_diameter = orifice diameter (m)
+    // double Pin = inlet pressure in Pa 
+    // double density = upstream density (kg / m^3)
+    // double MFR = mass flow rate through the orifice (kg/s)
+    // double coe = nozzle flow coefficient (dimensionless)
+    // OrificeFlowCalc(MFR, Pin, orifice_diameter, density, beta, coe)
+    
+    OrificeFlowCalc(MFR = 0, Pin = 0, orifice_diameter = 0, density = 0, beta = 0, coe = 0);
+    std::cout << "MFR = " << MFR << std::endl << "Pin = " << Pin << std::endl << "orifice_diameter = " << orifice_diameter << std::endl << "density = " << density << std::endl << "beta = " << beta << std::endl << "coe = " << coe << std::endl;
+    
+    OrificeFlowCalc( MFR = 1, Pin = 1000000 , orifice_diameter = 0.2, density = 14.0, beta = 0 , coe = 0);
+    std::cout << "MFR = " << MFR << std::endl << "Pin = " << Pin << std::endl << "orifice_diameter = " << orifice_diameter << std::endl << "density = " << density << std::endl << "beta = " << beta << std::endl << "coe = " << coe << std::endl;
+    
+    
+    return 0;
 }
