018d207168ab83038acce6026c9386c4e2726366
AAGAN <AAGAN@users.noreply.github.com>
2017-06-16 09:18:21 -0400

    Test unit for ORFLOWCALC method

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <assert.h>
+#include "OrificeFlowCalc.h"
 
 int main()
 {
@@ -11,13 +12,23 @@ int main()
     // double MFR = mass flow rate through the orifice (kg/s)
     // double coe = nozzle flow coefficient (dimensionless)
     // OrificeFlowCalc(MFR, Pin, orifice_diameter, density, beta, coe)
-    
-    OrificeFlowCalc(MFR = 0, Pin = 0, orifice_diameter = 0, density = 0, beta = 0, coe = 0);
-    std::cout << "MFR = " << MFR << std::endl << "Pin = " << Pin << std::endl << "orifice_diameter = " << orifice_diameter << std::endl << "density = " << density << std::endl << "beta = " << beta << std::endl << "coe = " << coe << std::endl;
+
+	double MFR = 0.0;
+	double Pin = 0.0;
+	double orifice_diameter = 0.0;
+	double density = 0.0;
+	double beta = 0.0;
+	double coe = 0.0;
+	    
+    OrificeFlowCalc(MFR = 0, Pin = 1160.0 * 6895.0, orifice_diameter = 0.205 * 0.0254, density = 10.10837 * 16.018, beta = 0.329582, coe = 0);
+    std::cout << "MFR = " << MFR << std::endl << "Pin = " << Pin << std::endl << "orifice_diameter = " 
+		<< orifice_diameter << std::endl << "density = " << density << std::endl << "beta = " 
+		<< beta << std::endl << "coe = " << coe << std::endl << std::endl;
     
     OrificeFlowCalc( MFR = 1, Pin = 1000000 , orifice_diameter = 0.02, density = 14.0, beta = 0 , coe = 0);
-    std::cout << "MFR = " << MFR << std::endl << "Pin = " << Pin << std::endl << "orifice_diameter = " << orifice_diameter << std::endl << "density = " << density << std::endl << "beta = " << beta << std::endl << "coe = " << coe << std::endl;
-    
-    
+    std::cout << "MFR = " << MFR << std::endl << "Pin = " << Pin << std::endl << "orifice_diameter = " 
+		<< orifice_diameter << std::endl << "density = " << density << std::endl << "beta = " 
+		<< beta << std::endl << "coe = " << coe << std::endl << std::endl;
+        
     return 0;
 }
